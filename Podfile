e6c9831d4926d5d7a4c1dd0ea1e9453a345b09e8
lichuanjun <lichuanjun@niwodai.net>
2017-10-17 14:45:42 +0800

    添加第三方库Masonry

---
+++ Podfile
@@ -0,0 +1,9 @@
+
+platform :ios, '8.0'
+
+workspace 'TLFinancialCharts'
+
+target 'TLFinancialCharts' do
+    pod 'Masonry', '~> 1.0.1'
+
+end
--- TLFinancialCharts/FinancialCharts/FCGridChartViewController.m
+++ TLFinancialCharts/FinancialCharts/FCGridChartViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "FCGridChartViewController.h"
+#import "CCSGridChart.h"
 
 @interface FCGridChartViewController ()
 
@@ -14,15 +15,54 @@
 
 @implementation FCGridChartViewController
 
-- (void)viewDidLoad {
-    [super viewDidLoad];
-    // Do any additional setup after loading the view.
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil {
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
 }
-
+    
 - (void)viewWillAppear:(BOOL)animated {
     [super viewWillAppear:animated];
     
     self.title = @"Grid Chart";
+}
+    
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    self.view.backgroundColor = [UIColor whiteColor];
+    
+    CCSGridChart *gridchart = [[CCSGridChart alloc] initWithFrame:CGRectMake(0, MARGIN_TOP, 360, 320)];
+    
+    gridchart.autoresizingMask = UIViewAutoresizingFlexibleLeftMargin | UIViewAutoresizingFlexibleTopMargin | UIViewAutoresizingFlexibleHeight | UIViewAutoresizingFlexibleWidth;
+    
+    gridchart.backgroundColor = [UIColor clearColor];
+    
+    NSMutableArray *TitleX = [[NSMutableArray alloc] init];
+    
+    [TitleX addObject:@"11/26"];
+    [TitleX addObject:@"12/3"];
+    [TitleX addObject:@"12/10"];
+    [TitleX addObject:@"12/17"];
+    [TitleX addObject:@"12/24"];
+    [TitleX addObject:@"12/31"];
+    [TitleX addObject:@"1/7"];
+    [TitleX addObject:@"1/14"];
+    
+    gridchart.longitudeTitles = TitleX;
+    
+    NSMutableArray *TitleY = [[NSMutableArray alloc] init];
+    
+    [TitleY addObject:@"0"];
+    [TitleY addObject:@"1000"];
+    [TitleY addObject:@"2000"];
+    [TitleY addObject:@"3000"];
+    [TitleY addObject:@"4000"];
+    
+    gridchart.latitudeTitles = TitleY;
+    
+    [self.view addSubview:gridchart];
 }
     
 - (void)didReceiveMemoryWarning {
